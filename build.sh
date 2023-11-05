a73925c4a84d0b863958d7ba62b89d2c45f9cd94
paging <94936050+ida64@users.noreply.github.com>
2023-11-05 12:38:45 -0600

    Update build.sh
    Signed-off-by: paging <94936050+ida64@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -5,12 +5,18 @@ then
     exit
 fi
 
+echo "Found conan, installing dependencies..."
+
 # Install dependencies
 conan install .
+
+echo "Building debug..."
 
 # Build Debug
 cmake -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=conan_provider.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=install -B build -S .
 cmake --build build --config Debug --target ALL_BUILD
+
+echo "Building release..."
 
 # Build Release
 cmake -DCMAKE_PROJECT_TOP_LEVEL_INCLUDES=conan_provider.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -B build -S .

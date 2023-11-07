c50c2858079c17cf78515c12abd5a668f1bcb468
paging <paging@DESKTOP-A5QCNE5>
2023-11-07 02:49:36 -0600

    BaseDefs.h refactor

--- Source/Common/Base/BaseDefs.h
+++ Source/Common/Base/BaseDefs.h
@@ -9,34 +9,20 @@
 // Types
 //
 
-// Signed 8-bit integer
 typedef signed char vgChar;
-// Unsigned 8-bit integer
 typedef unsigned char vgUChar;
-// Unsigned 8-bit integer
 typedef unsigned char vgByte;
 typedef unsigned char vgU8;
-// Signed 8-bit integer
 typedef signed char vgS8;
-// Signed 16-bit integer
 typedef signed short vgShort;
-// Unsigned 16-bit integer
 typedef unsigned short vgUShort;
-// Signed 32-bit integer
 typedef signed int vgS32;
-// Unsigned 32-bit integer
 typedef unsigned int vgU32;
-// Signed 64-bit integer
 typedef signed long long vgS64;
-// Unsigned 64-bit integer
 typedef unsigned long long vgU64;
-// 32-bit floating point
 typedef float vgFloat;
-// 64-bit floating point
 typedef double vgDouble;
-// 32-bit boolean
 typedef bool vgBool;
-// const char*
 typedef const char* vgString;
 
 //
@@ -45,7 +31,6 @@ typedef const char* vgString;
 
 #define VG_ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
-// Inline macro
 #ifdef _MSC_VER
 #define VG_INLINE __forceinline
 #else
@@ -55,7 +40,7 @@ typedef const char* vgString;
 #define VG_FROM_PTR_UNSAFE(Name) Name.get()
 
 //
-// Attributes and Annotations
+// Markers
 //
 
 
